9ae5ea370c6c014ba8464973c6c655165335562d
Amine Anouja <amine.anouja@fr.ibm.com>
2021-01-17 18:57:48 +0400

    simple ibm cloud vsi

--- custom/classic-vsi/classic-vsi.tf
+++ custom/classic-vsi/classic-vsi.tf
@@ -1,8 +1,12 @@
+variable "dc" {
+  type = "string"
+}
+
 resource "ibm_compute_vm_instance" "vm1" {
     hostname             = "vm1"
     domain               = "example.com"
     os_reference_code    = "UBUNTU_18_64"
-    datacenter           = "fra02"
+    datacenter           = "${var.dc}"
     network_speed        = 10
     hourly_billing       = true
     private_network_only = false
@@ -12,8 +16,8 @@ resource "ibm_compute_vm_instance" "vm1" {
     local_disk           = false
 }
 
-data "ibm_iam_users" "users_profiles"{
-}
+# data "ibm_iam_users" "users_profiles"{
+# }
 
 # output "user" {
 #   value = data.ibm_iam_users.users_profiles.users
